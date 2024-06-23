bb2e320a59449895b16530c3300dd56906b8674e
Postolache Andreea Miruna <79594745+postolache-andreea-miruna@users.noreply.github.com>
2024-06-23 15:35:27 +0300

    salesOff after lb

--- src/main/java/com/example/salesoff/controllers/SalesOffController.java
+++ src/main/java/com/example/salesoff/controllers/SalesOffController.java
@@ -2,24 +2,45 @@ package com.example.salesoff.controllers;
 
 import com.example.salesoff.config.PropertiesConfig;
 import com.example.salesoff.model.SalesOff;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class SalesOffController {
     @Autowired
     private PropertiesConfig propConfig;
 
-    @GetMapping("/salesOff")
-    public SalesOff getSalesOff(){
-        return new SalesOff(propConfig.getAuthors(), propConfig.getCategories(),
-                propConfig.getLowSalesOff(),
-                propConfig.getLowNoBooks(),
-                propConfig.getMediumSalesOff(),
-                propConfig.getMediumNoBooks(),
-                propConfig.getHighSalesOff(),
-                propConfig.getHighNoBooks(),
-                propConfig.getVersionId());
-    }
+//    @GetMapping("/salesOff")
+//    public SalesOff getSalesOff(){
+//        return new SalesOff(propConfig.getAuthors(), propConfig.getCategories(),
+//                propConfig.getLowSalesOff(),
+//                propConfig.getLowNoBooks(),
+//                propConfig.getMediumSalesOff(),
+//                propConfig.getMediumNoBooks(),
+//                propConfig.getHighSalesOff(),
+//                propConfig.getHighNoBooks(),
+//                propConfig.getVersionId());
+//    }
+
+    //zipkin
+@GetMapping("/salesOff")
+public ResponseEntity<SalesOff> getSalesOff(@RequestHeader("awbd-id")
+                            String correlationId){
+    SalesOff salesOff= new SalesOff(propConfig.getAuthors(), propConfig.getCategories(),
+            propConfig.getLowSalesOff(),
+            propConfig.getLowNoBooks(),
+            propConfig.getMediumSalesOff(),
+            propConfig.getMediumNoBooks(),
+            propConfig.getHighSalesOff(),
+            propConfig.getHighNoBooks(),
+            propConfig.getVersionId());
+    log.info("correlation-id discount: {}", correlationId);
+    return ResponseEntity.status(HttpStatus.OK).body(salesOff);
+}
 }
